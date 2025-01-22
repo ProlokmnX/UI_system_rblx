bbf010424a96aff137ff6e4ffa2de030a00534a6
ProlokmnX <prolokmnx@gmail.com>
2025-01-22 17:55:05 +0100

    Update main_ui.lua

--- main_ui.lua
+++ main_ui.lua
@@ -1,7 +1,6 @@
 local MainUI = {}
 
 local Button = loadstring(game:HttpGet("https://raw.githubusercontent.com/ProlokmnX/UI_system_rblx/main/button.lua"))()
-local Slider = loadstring(game:HttpGet("https://raw.githubusercontent.com/ProlokmnX/UI_system_rblx/main/slider.lua"))()
 local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/ProlokmnX/UI_system_rblx/main/ui.lua"))()
 
 function MainUI.create(title, titleColor, bgColor, transparency, iconId)
@@ -12,9 +11,6 @@ function MainUI.create(title, titleColor, bgColor, transparency, iconId)
     return {
         addButton = function(text, textColor, bgColor, transparency, iconId, callback)
             Button.create(mainFrame, text, textColor, bgColor, transparency, iconId, callback)
-        end,
-        addSlider = function(min, max, defaultValue, callback)
-            Slider.create(mainFrame, min, max, defaultValue, callback)
         end,
     }
 end
